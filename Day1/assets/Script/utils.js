457331509c703b397d035a3396f9dc3bf7f84e8d
kakalak11 <92971472+kakalak11@users.noreply.github.com>
2021-12-03 17:40:31 +0700

    update

--- Day1/assets/Script/utils.js
+++ Day1/assets/Script/utils.js
@@ -1,1 +1,16 @@
 console.log('Hello world');
+
+function primeNumber(number) {
+    let array = Array(number).fill(true);
+    let primeArray = [];
+
+    for (let i = 2; i < number; i++) {
+        if (array[i] === true) {
+            primeArray.push(i);
+            for (let j = i * i; j < number; j += i) {
+                array[j] = 0
+            }
+        }
+    }
+    return primeArray;
+}
